158388c83bbb3ef3af212168d3f16e07fda2c0a4
Thibaut Cuvelier <cuvelier.thibaut@gmail.com>
2019-05-08 17:56:29 +0200

    Test suite for ExploreThenCommitBandit.

--- skbandit/bandit.py
+++ skbandit/bandit.py
@@ -14,7 +14,7 @@ class Bandit(ABC):
       not have to, be called after each call to `pull()`
     """
 
-    def __init__(self, n_arms):
+    def __init__(self, n_arms: int):
         self.n_arms = n_arms
 
     @abstractmethod
@@ -28,12 +28,20 @@ class Bandit(ABC):
 
 class RewardAccumulatorMixin:
     def __init__(self, n_arms: int):
-        self.arm_counts = [0] * n_arms
-        self.total_rewards = [0.0] * n_arms
+        self._arm_counts = [0] * n_arms
+        self._total_rewards = [0.0] * n_arms
 
     def reward(self, arm: int, reward: float) -> None:
-        self.arm_counts[arm] += 1
-        self.total_rewards[arm] += reward
+        self._arm_counts[arm] += 1
+        self._total_rewards[arm] += reward
+
+    @property
+    def total_rewards(self):
+        return self._total_rewards
+
+    @property
+    def arm_counts(self):
+        return self._arm_counts
 
 
 class ExploreThenCommitBandit(Bandit, RewardAccumulatorMixin):
@@ -50,11 +58,13 @@ class ExploreThenCommitBandit(Bandit, RewardAccumulatorMixin):
 
     def __init__(self, n_arms: int, n_epochs: Union[int, None] = None, gap: Union[float, None] = None,
                  horizon: Union[int, None] = None):
-        super().__init__(n_arms)
+        Bandit.__init__(self, n_arms)
+        RewardAccumulatorMixin.__init__(self, n_arms)
+
         self.current_round = 0
         self.best_arm = None
 
-        if gap is not None and gap is not None:
+        if gap is not None and horizon is not None:
             self.n_epochs = max(1, math.ceil((4 / gap ** 2) * math.log(horizon * gap ** 2 / 4)))
             self.n_remaining_epochs = self.n_epochs
         elif n_epochs is not None:
@@ -64,22 +74,26 @@ class ExploreThenCommitBandit(Bandit, RewardAccumulatorMixin):
             self.n_epochs = 1
             self.n_remaining_epochs = 1
 
+    def _next_round(self):
+        self.current_round += 1
+        if self.current_round % self.n_arms == 0:
+            self.n_remaining_epochs -= 1
+
     def pull(self) -> int:
         # Exploration phase: explore once each arm.
         if self.current_round < self.n_arms * self.n_epochs:
-            return self.current_round % self.n_arms
+            arm = self.current_round % self.n_arms
         # Exploitation phase: always return the best arm (computed only once).
         elif self.best_arm is not None:
-            return self.best_arm
+            arm = self.best_arm
         else:
             self.best_arm = self.total_rewards.index(max(self.total_rewards))
-            return self.best_arm
+            arm = self.best_arm
+
+        self._next_round()
+        return arm
 
     def reward(self, arm: int, reward: float) -> None:
         # Stop storing rewards after the exploration phase.
-        if self.current_round < self.n_arms:
-            super().reward(arm, reward)
-
-            self.current_round += 1
-            if self.current_round % self.n_arms == 0:
-                self.n_remaining_epochs -= 1
+        if self.current_round <= self.n_arms * self.n_epochs:
+            RewardAccumulatorMixin.reward(self, arm, reward)
--- tests/tests.py
+++ tests/tests.py
@@ -1,1 +1,99 @@
 import unittest
+from skbandit.bandit import ExploreThenCommitBandit
+
+
+class TestExploreThenCommitBandit(unittest.TestCase):
+    def test_one_epoch(self):
+        b = ExploreThenCommitBandit(n_arms=2)
+
+        # Exploration.
+        self.assertEqual(b.pull(), 0)
+        self.assertEqual(b.pull(), 1)
+
+        # Exploitation.
+        arm = b.pull()  # Compute best arm.
+        self.assertIn(arm, [0, 1])
+        self.assertEqual(b.pull(), arm)
+        self.assertEqual(b.pull(), arm)
+        self.assertEqual(b.pull(), arm)
+
+    def test_fixed_epoch(self):
+        b = ExploreThenCommitBandit(n_arms=2, n_epochs=3)
+
+        # Exploration.
+        self.assertEqual(b.pull(), 0)
+        self.assertEqual(b.pull(), 1)
+        self.assertEqual(b.pull(), 0)
+        self.assertEqual(b.pull(), 1)
+        self.assertEqual(b.pull(), 0)
+        self.assertEqual(b.pull(), 1)
+
+        # Exploitation.
+        arm = b.pull()  # Compute best arm.
+        self.assertIn(arm, [0, 1])
+        self.assertEqual(b.pull(), arm)
+        self.assertEqual(b.pull(), arm)
+        self.assertEqual(b.pull(), arm)
+
+    def test_computed_epoch(self):
+        b = ExploreThenCommitBandit(n_arms=2, gap=1.0, horizon=10)
+        # ceil(4 / 1^2 * ln(10 * 1^2 / 4)) = 4
+
+        # Exploration.
+        self.assertEqual(b.pull(), 0)
+        self.assertEqual(b.pull(), 1)
+        self.assertEqual(b.pull(), 0)
+        self.assertEqual(b.pull(), 1)
+        self.assertEqual(b.pull(), 0)
+        self.assertEqual(b.pull(), 1)
+        self.assertEqual(b.pull(), 0)
+        self.assertEqual(b.pull(), 1)
+
+        # Exploitation.
+        arm = b.pull()  # Compute best arm.
+        self.assertIn(arm, [0, 1])
+        self.assertEqual(b.pull(), arm)
+        self.assertEqual(b.pull(), arm)
+        self.assertEqual(b.pull(), arm)
+
+    def test_one_epoch_rewarded(self):
+        b = ExploreThenCommitBandit(n_arms=3)
+
+        # Exploration.
+        self.assertEqual(b.pull(), 0)
+        b.reward(0, 10)
+        self.assertEqual(b.pull(), 1)
+        b.reward(1, 0)
+        self.assertEqual(b.pull(), 2)
+        b.reward(2, 5)
+
+        # Exploitation.
+        self.assertEqual(b.pull(), 0)
+        self.assertEqual(b.pull(), 0)
+        self.assertEqual(b.pull(), 0)
+        self.assertEqual(b.pull(), 0)
+
+        # RewardAccumulatorMixin.
+        self.assertEqual(b.total_rewards, [10.0, 0.0, 5.0])
+        self.assertEqual(b.arm_counts, [1, 1, 1])
+
+        # Even if giving more rewards for another arm, no change in exploitation phase.
+        # This use is outside the expected use of the bandit.
+        b.reward(0, 0)
+        b.reward(1, 0)
+        b.reward(2, 15)
+        b.reward(0, 0)
+        b.reward(1, 0)
+        b.reward(2, 45)
+        b.reward(0, 0)
+        b.reward(1, 0)
+        b.reward(2, 35)
+
+        self.assertEqual(b.pull(), 0)
+        self.assertEqual(b.pull(), 0)
+        self.assertEqual(b.pull(), 0)
+        self.assertEqual(b.pull(), 0)
+
+        # RewardAccumulatorMixin did not see any change.
+        self.assertEqual(b.total_rewards, [10.0, 0.0, 5.0])
+        self.assertEqual(b.arm_counts, [1, 1, 1])
